3a09faf35c82ecb889c9361dfb1115e4fb185311
RenardFake <rmacorol18@gmail.com>
2024-03-28 11:05:55 +0800

    Quit button add

--- GUI.py
+++ GUI.py
@@ -1,6 +1,7 @@
 from tkinter import * #import tkinter
 
 window = Tk() #instace a window 
+standardtext =40 ## standard text size
 window.geometry("1280x750")  # set size of a window
 window.title("Machine Problem #3")
 window.config(background="#dadef4") #voilet tokyo  
@@ -12,13 +13,28 @@ group_members = Label(window,
               bg='#8590c5') #Background Color
 group_members.place(x=415,y=30) 
 
+
+
 group_List = "Macorol, \nCampos, \nTamayo, \nBandong"
 group_ListVar = Message(window, 
                         text=group_List)
 group_ListVar.config(bg="#fffcb0", 
-                     font=('RobotoMono',40,'bold'),
+                     font=('RobotoMono',standardtext,'bold'),
                      fg='#000000')
 group_ListVar.place(x=415,y=100)
+
+def click():
+    window.quit()
+    
+
+button = Button(window,
+                text="Quit",
+                command=click,
+                bg="#fffcb0", 
+                font=('RobotoMono',40,'bold'),
+                fg='#000000')
+
+button.place(x=500,y=500)
 
 
 window.mainloop() # set visible 
